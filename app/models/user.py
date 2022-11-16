10fb16560f46329b4cef8dbffd524da02e6fd9d5
Eric-Peltzman33 <erpeltz@gmail.com>
2022-11-16 01:29:01 -0500

    updated user table

--- app/models/user.py
+++ app/models/user.py
@@ -14,6 +14,9 @@ class User(db.Model, UserMixin):
         username = db.Column(db.String(40), nullable=False, unique=True)
         email = db.Column(db.String(255), nullable=False, unique=True)
         hashed_password = db.Column(db.String(255), nullable=False)
+        created_at = db.Column(db.DateTime, server_default=db.func.now())
+        updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+         
        
         
          
