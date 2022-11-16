03329df8dc6e636f0dce951bc225db5a62073bdb
epeltz33 <epeltz@optonline.net>
2022-11-16 03:01:16 +0000

    config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,13 @@
 import os 
+
+
+class Config: 
+     """Set Flask configuration vars from .env file.""" 
+     # General Config 
+     SECRET_KEY = os.environ.get('SECRET_KEY') 
+     
+        # Database Config
+        # the .replace() is for when you deploy to Heroku  
+     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
+     SQLALCHEMY_ECHO = True # prints out the SQL queries that are run in the terminal
+     SQLALCHEMY_TRACK_MODIFICATIONS = False  # silence the deprecation warning  
