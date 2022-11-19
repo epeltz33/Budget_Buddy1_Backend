b161825767317978d9ee94cfc2c35a1665b78d2f
epeltz33 <epeltz@optonline.net>
2022-11-19 03:32:30 +0000

    transaction payee

--- app/api/transaction_routes.py
+++ app/api/transaction_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from app.models import db, Transaction
+from app.models import db, Transaction, Account
 from flask_login import current_user, login_required
 from sqlalchemy import func
 
@@ -11,5 +11,12 @@ transaction_routes = Blueprint('transactions', __name__)
 @transaction_routes.route('/')
 @login_required
 def all_transactions():
-	transactions = Transaction.query.filter(Transaction.user_id == current_user.id).all()
-	return {"transactions": [transaction.to_dict() for transaction in transactions]}
+ transactions = Transaction.query.join(Account).filter(
+     Account.userId == current_user.get_id()).order_by(Transaction.trans_date.desc())
+
+ return {'all_transactions': [transaction.to_dict() for transaction in transactions]}
+
+@transaction_routes.route('/', methods=['POST'])
+@login_required
+def add_transaction():
+    trans_payee = request.json['trans_payee'], # trans_payee is a string of the payee name
