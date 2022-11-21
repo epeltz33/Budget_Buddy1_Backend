62d1a7b88f85e4b31aea615b6b25af8c9ef008d5
Eric-Peltzman33 <erpeltz@gmail.com>
2022-11-20 21:41:33 -0500

    setting up store

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import configureStore from './store';
+const store = configureStore();
+//  Wrap the App component with the Provider component and pass the store as a prop to the Provider component.
+ReactDOM.render( 
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
