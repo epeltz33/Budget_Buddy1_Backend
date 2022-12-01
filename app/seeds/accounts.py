a3b58a7ac4ed72a49cf47b58b2b1645ce78c7822
epeltz33 <epeltz@optonline.net>
2022-12-01 16:01:23 -0500

    updated budgets seed

--- app/seeds/accounts.py
+++ app/seeds/accounts.py
@@ -2,17 +2,21 @@ from app.models import db, Account
 
 
 def seed_accounts(): # this is the function that will seed the accounts for the database and the accounts table 
-    amex = Account(account_name='American Express', account_balance=10000.00, userId=1)
-    chase = Account(account_name='Chase',  account_balance=20000.00, userId=1)
-    wells = Account(account_name='Wells Fargo',  account_balance=30000.00, userId=1)
+    amex = Account(account_name='American Express', userId=1)
+    chase = Account(account_name='Chase', userId=1)
+    discover = Account(account_name='Discover', userId=1)
+    capitalOne = Account(account_name='Capital One', userId=1)
+    citi = Account(account_name='Citi', userId=1)
     
-    db.session.add(amex) # this is the function that will add the accounts to the database session
+    db.session.add(amex)
     db.session.add(chase)
-    db.session.add(wells)
-     # db.session.commit() # this is the function that will commit the database session  
-     
-def undo_accounts(): # this is the function that will undo the accounts for the database and the accounts table
-    db.session.execute('TRUNCATE accounts RESTART IDENTITY CASCADE;') # truncate the accounts table and restart the identity
-    db.session.commit() # commit the database session 
-
+    db.session.add(discover)
+    db.session.add(capitalOne)
+    db.session.add(citi)
+    
+    db.session.commit()
+    
+def undo_accounts():
+    db.session.execute('TRUNCATE accounts RESTART IDENTITY CASCADE;')
+    db.session.commit()
     
--- app/seeds/budgets.py
+++ app/seeds/budgets.py
@@ -1,40 +1,18 @@
 from app.models import db, Budget
-from datetime import datetime
 
-def seed_budgets():
-  # use the data from transactions.py to seed the budgets table 
-    # with the data from transactions.py
- Utilities = Budget(
-    account_id=1, 
-    budget_amount=300.00,
-    budget_date=datetime(2021, 1, 1),
-    categoryId=1,
-    )
- Food = Budget(
-    account_id=1,
-    budget_amount=400.00,
-    budget_date=datetime(2021, 1, 2),
-    categoryId=2,
-    )
- Entertainment = Budget(
-    account_id=1,
-    budget_amount=500.00,
-    budget_date=datetime(2021, 7, 3),
-    categoryId=3,
-    )
- Transportation = Budget(
-    account_id=1,
-    budget_amount=600.00,
-    budget_date=datetime(2021, 2, 4),
-    categoryId=3,
-    )
- 
- db.session.add(Utilities)
- db.session.add(Food)
- db.session.add(Entertainment)
- db.session.add(Transportation)
- db.session.commit()
- 
+def seed_budgets(): # this is the function that will seed the budgets for the database and the budgets table 
+   shopping = Budget(budget_name='Shopping', userId=1, budget_amount=1000, categoryId=19)
+   dining = Budget(budget_name='Dining', userId=1, budget_amount=1000, categoryId=6)
+   total = Budget(budget_name='Total', budget_amount=2500, categoryId=1, userId=1)
+   groceries = Budget(budget_name='Groceries', userId=1, budget_amount=1000, categoryId=13)
+   
+   db.session.add(shopping)
+   db.session.add(dining)
+   db.session.add(total)
+   db.session.add(groceries)
+   
+   db.session.commit()
+   
 def undo_budgets():
-    db.session.execute('TRUNCATE budgets RESTART IDENTITY CASCADE;')
-    db.session.commit()
+   db.session.execute('TRUNCATE budgets RESTART IDENTITY CASCADE;')
+   db.session.commit()
